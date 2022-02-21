1d84b1847a207fd2f93164c6b2c06375c33e6f5c
Musatov Vladimir <vladimir.pmz@gmail.com>
2022-02-21 18:57:22 +0300

    Initial Commit

---
+++ H.W. Lantern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H.W. Lantern
+//
+//  Created by влад on 21.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
